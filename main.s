e42e7c7e468df87b0554cb6032f1f6fe125726e9
Arthur Pellegrini <arthur.pellegrini@outlook.fr>
2022-11-21 13:24:34 +0100

    SWITCHS
    Co-Authored-By: Clément <90420417+Clem-0000@users.noreply.github.com>

--- main.s
+++ main.s
@@ -1,6 +1,6 @@
 	
 	;; RK - Evalbot (Cortex M3 de Texas Instrument)
-	;; fait clignoter une seule LED connectée au port GPIOF
+	;; fait clignoter une seule LED connectï¿½e au port GPIOF
    	
 		AREA    |.text|, CODE, READONLY
  
@@ -8,6 +8,8 @@
 SYSCTL_PERIPH_GPIOF EQU		0x400FE108	; SYSCTL_RCGC2_R (p291 datasheet de lm3s9b92.pdf)
 
 ; The GPIODATA register is the data register
+GPIO_PORTD_BASE		EQU		0x40007000	; GPIO Port D (APB) base: 0x4000.7000
+GPIO_PORTE_BASE		EQU		0x40024000	; GPIO Port E (APB) base: 0x4002.4000
 GPIO_PORTF_BASE		EQU		0x40025000	; GPIO Port F (APB) base: 0x4002.5000 (p416 datasheet de lm3s9B92.pdf)
 
 ; configure the corresponding pin to be an output
@@ -22,9 +24,16 @@ GPIO_O_DR2R   		EQU 	0x00000500  ; GPIO 2-mA Drive Select (p428 datasheet de lm3
 ; To use the pin as a digital input or output, the corresponding GPIODEN bit must be set.
 GPIO_O_DEN   		EQU 	0x0000051C  ; GPIO Digital Enable (p437 datasheet de lm3s9B92.pdf)
 
-; PIN select
-PINS_PORTF				EQU		0x30		; led 1 & 2 sur broches 4 & 5 (0011 0000)
+; The GPIOPUR register is the pull-up control register
+GPIO_O_PUR			EQU		0x00000510 
 
+; PINS port input
+PINSD_6_7				EQU		0xC0		; switch 1 & 2 sur broches 6 & 7 (1100 0000)
+PINSE_1_2				EQU		0x03		; bumpers 1 & 2 sur les broches (0000 0011)
+
+; PINS port output
+PINSF_4_5				EQU		0x30		; led 1 & 2 sur broches 4 & 5 (0011 0000)
+	
 ; blinking frequency
 DUREE   			EQU     0x002FFFFF	; Random Value
 
@@ -35,20 +44,20 @@ __main
 		; ;; Enable the Port F peripheral clock by setting bit 5 (0x20 == 0b100000)		(p291 datasheet de lm3s9B96.pdf)
 		; ;;														 (GPIO::FEDCBA)
 		ldr r6, = SYSCTL_PERIPH_GPIOF  			;; RCGC2
-        mov r0, #0x00000020  					;; Enable clock sur GPIO F où sont branchés les leds (0x20 == 0b100000)
+        mov r0, #0x00000038  					;; Enable clock sur GPIO F E D (0011 1000)
 		; ;;														 									 (GPIO::FEDCBA)
         str r0, [r6]
-		
+	
 		; ;; "There must be a delay of 3 system clocks before any GPIO reg. access  (p413 datasheet de lm3s9B92.pdf)
 		nop	   									;; tres tres important....
 		nop	   
 		nop	   									;; pas necessaire en simu ou en debbug step by step...
 	
 		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION LED
-        ldr r0, = PINS_PORTF
+        ldr r0, = PINSF_4_5
         ldr r6, = GPIO_PORTF_BASE+GPIO_O_DIR   
         str r0, [r6]
-		
+		    
         ldr r6, = GPIO_PORTF_BASE+GPIO_O_DEN
         str r0, [r6]
  
@@ -56,35 +65,45 @@ __main
         str r0, [r6]
 		
 		; allumer les leds
-		mov r3, #PINS_PORTF       					;; Allume portF broches 4 et 5
-		ldr r6, = GPIO_PORTF_BASE + (PINS_PORTF<<2) 
-		
-		        
-        mov r2, #0x000       					;; pour eteindre LED
+		mov r3, #PINSF_4_5    
+
+		;; Allume portF broches 4 et 5   					
+		ldr r6, = GPIO_PORTF_BASE + (PINSF_4_5<<2) 
+
+		;; pour eteindre LED 
+        mov r2, #0x000       					
 
 		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration LED 
 		
 		
 		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CONFIGURATION SWITCH
+		ldr r0, = PINSD_6_7
 		
+		ldr r7, = GPIO_PORTD_BASE+GPIO_O_DEN
+		str r0, [r7]
+ 
+		ldr r7, = GPIO_PORTD_BASE+GPIO_O_PUR	
+		str r0, [r7]
 		;vvvvvvvvvvvvvvvvvvvvvvvFin configuration SWITCH
 		
 
 		;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CLIGNOTTEMENT
 
-loop    str r2, [r6]    						;; Eteint LED car r2 = 0x00     		
+loop    
+		ldr r1, [r7]							;; on rÃ©cupÃ¨re les entrÃ©es des switchs
+		str r2, [r6]    						;; Eteint LED car r2 = 0x00     		
         
-		ldr r1, = DUREE
-wait1	subs r1, #1
+		ldr r0, = DUREE
+wait1	subs r0, #1
         bne wait1
 	
         str r3, [r6]  							;; Allume portF broche 4 & 5 : 00110000 (contenu de r3)
         
-		ldr r1, = DUREE
-wait2   subs r1, #1
+		ldr r0, = DUREE
+wait2   subs r0, #1
         bne wait2
 
         b loop       
 		
 		nop		
-        END 
+		END
